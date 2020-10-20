6c9dd5ce9f324f5ea09184798e09a01c886acc3b
Prottay Rudra <prottoyrudra7777@gmail.com>
2020-10-20 22:02:01 +0600

    Updated content - added seo

---
+++ pages/_document.js
@@ -0,0 +1,41 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+          <meta
+            httpEquiv="Content-Type"
+            content="text/html; charset=ISO-8859-1"
+          />
+          <meta name="description" content="A webapp which's screen is white" />
+
+          <meta property="og:type" content="website" />
+          <meta
+            property="og:image"
+            content="https://i.ibb.co/m84C7kK/Untitled-design-8.jpg"
+          />
+          <meta
+            property="og:description"
+            content="A webapp which's screen is white"
+          />
+          <meta name="twitter:card" content="summary_large_image" />
+          <link rel="shortcut icon" href="/favicon.jpg" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>White</title>
+        <meta property="og:title" content="White" />
+      </Head>
+      <div>
+        <h1>White Background</h1>
+      </div>
+    </>
+  );
+}
